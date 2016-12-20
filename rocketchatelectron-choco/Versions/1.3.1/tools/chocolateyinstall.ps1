4cb451d7e49efdb477a847390ebf5fa65ddb8327
Zhang Cheng <13501393281@163.com>
2016-12-20 16:45:37 +0000

    add rocketchat

---
+++ rocketchatelectron-choco/Versions/1.3.1/tools/chocolateyinstall.ps1
@@ -0,0 +1,2 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+Install-ChocolateyPackage -packageName 'rocket.chat' -FileType exe-SilentArgs '/S'-Url64 'https://github.com/RocketChat/Rocket.Chat.Electron/releases/download/1.3.1/rocketchat-v1.3.1-win32-x64.exe' -checksum64 '25C56001CF6BC3082B8B6E7183052DD9410A69B1E9095F8CBA7C73B9F60B133D' -checksumType64 'sha256'
