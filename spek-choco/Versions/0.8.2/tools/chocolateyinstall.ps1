82b90ae483141706a1ae1a91b68a0a39c14ff677
Zhang Cheng <13501393281@163.com>
2016-12-21 11:13:14 +0000

    update to 0.8.2

---
+++ spek-choco/Versions/0.8.2/tools/chocolateyinstall.ps1
@@ -0,0 +1,4 @@
+﻿
+$ErrorActionPreference = 'Stop'; # stop on all errors
+
+Install-ChocolateyPackage -packageName 'spek' -FileType exe -SilentArgs '/quiet' -Url 'https://github.com/alexkay/spek/releases/download/v0.8.2/spek-0.8.2.msi' -Checksum "3E6956704E9198F2D5EF32E287AE9B4AC556DFF052ACE13DFC44AD1CED55F6AA" -ChecksumType "sha256"
