2796bd5ad22f216b7105e673452aa3517b2de1a1
Zhang Cheng <13501393281@163.com>
2016-12-21 11:18:15 +0000

    fix the installer type

--- spek-choco/Versions/0.8.2/tools/chocolateyinstall.ps1
+++ spek-choco/Versions/0.8.2/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
 ﻿
 $ErrorActionPreference = 'Stop'; # stop on all errors
 
-Install-ChocolateyPackage -packageName 'spek' -FileType exe -SilentArgs '/quiet' -Url 'https://github.com/alexkay/spek/releases/download/v0.8.2/spek-0.8.2.msi' -Checksum "0FA1237597E9B1C966DF5701BC0A3DB88A7545C56F468BF6A21215ED4ABEA693" -ChecksumType "sha256"
+Install-ChocolateyPackage -packageName 'spek' -FileType msi -SilentArgs '/quiet' -Url 'https://github.com/alexkay/spek/releases/download/v0.8.2/spek-0.8.2.msi' -Checksum "0FA1237597E9B1C966DF5701BC0A3DB88A7545C56F468BF6A21215ED4ABEA693" -ChecksumType "sha256"
