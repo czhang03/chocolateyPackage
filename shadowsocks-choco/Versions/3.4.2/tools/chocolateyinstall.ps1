46f4448d97eeed1518ea3d6991deecf69ea6d4df
Zhang Cheng <13501393281@163.com>
2016-12-21 15:10:16 +0000

    update shadowsocks to 3.4.2

--- shadowsocks-choco/Versions/3.4.2/tools/chocolateyinstall.ps1
+++ shadowsocks-choco/Versions/3.4.2/tools/chocolateyinstall.ps1
@@ -1,1 +1,3 @@
-﻿Install-ChocolateyZipPackage -packageName 'shadowsocks' -UnzipLocation $(Split-Path -Parent $MyInvocation.MyCommand.Definition) -Url 'https://github.com/shadowsocks/shadowsocks-windows/releases/download/3.4.2/Shadowsocks-3.4.2.zip'-Checksum '078A26A4973FC2C7B358FB9CCC560226D8706F7D4454C2BA8A4FA4BE90FDE87A' -ChecksumType 'sha256'
+﻿
+$ErrorActionPreference = 'Stop'; # stop on all errors
+Install-ChocolateyZipPackage -packageName 'shadowsocks' -UnzipLocation $(Split-Path -Parent $MyInvocation.MyCommand.Definition) -Url 'https://github.com/shadowsocks/shadowsocks-windows/releases/download/3.4.2/Shadowsocks-3.4.2.zip' -checksum '078A26A4973FC2C7B358FB9CCC560226D8706F7D4454C2BA8A4FA4BE90FDE87A' -checksumType 'sha256'
