84e13767f9c35e0cb208f001bcfd99f9558a940b
Zhang Cheng <13501393281@163.com>
2016-12-22 19:13:36 +0000

    add bitdiffer

---
+++ bitdiffer-choco/Versions/1_5_0_4/tools/chocolateyinstall.ps1
@@ -0,0 +1,2 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+Install-ChocolateyZipPackage -packageName 'bitdiffer' -UnzipLocation $(Split-Path -Parent $MyInvocation.MyCommand.Definition) -Url 'https://github.com/bitdiffer/bitdiffer/releases/download/v1_5_0_4/BitDiffer_1_5_0_4.zip' -checksum '4011698ADCE30843DD4F6200D0E4CA23468F792140112CEA5580ECF4AE4BC474' -checksumType 'sha256'
