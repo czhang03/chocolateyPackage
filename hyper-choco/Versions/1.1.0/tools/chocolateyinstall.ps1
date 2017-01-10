ea80aa201e17b68703e16fbe4ebbc60a75c1c8e8
Zhang Cheng <13501393281@163.com>
2017-01-10 20:29:35 +0000

    udpate hyper to 1.1.0

---
+++ hyper-choco/Versions/1.1.0/tools/chocolateyinstall.ps1
@@ -0,0 +1,2 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+Install-ChocolateyPackage -packageName 'hyper' -FileType exe -SilentArgs '--silent' -Url64 'https://github.com/zeit/hyper/releases/download/1.1.0/hyper-Setup-1.1.0-win.exe' -checksum64 '340694087709BE0D79EBA30A3A070B11052486A42F79E8B7A270FC139BDB8FFA' -checksumType64 'sha256' 
