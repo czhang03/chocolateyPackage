3931ceea4a4e8919f402f11fbed6d05a826dc83d
Zhang Cheng <13501393281@163.com>
2017-01-14 19:33:39 +0000

    add dnspy package

---
+++ dnSpy-choco/Versions/3.0.0/tools/chocolateyinstall.ps1
@@ -0,0 +1,3 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+Install-ChocolateyZipPackage -packageName 'dnspy' -UnzipLocation $(Split-Path -Parent $MyInvocation.MyCommand.Definition) -Url 'https://github.com/0xd4d/dnSpy/releases/download/v3.0.0/dnSpy.zip' -checksum '99C0E7BDC93E8051E7E4F09FF75C38CE77119DE4A5434CD0CB6AAAF0F71A4CE2' -checksumType 'sha256'
