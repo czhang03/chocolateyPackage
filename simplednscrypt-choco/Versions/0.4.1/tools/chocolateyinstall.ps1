d572ae9084c5513ca774251f2fe7d07ff5c6fce7
Zhang Cheng <13501393281@163.com>
2017-01-22 16:27:02 +0000

    fix simple dnscrypt

--- simplednscrypt-choco/Versions/0.4.1/tools/chocolateyinstall.ps1
+++ simplednscrypt-choco/Versions/0.4.1/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
 ﻿
 $ErrorActionPreference = 'Stop'; # stop on all errors
 
-Install-ChocolateyPackage -packageName 'simplednscrpt' -FileType msi -SilentArgs '/quiet' -Url 'https://github.com/bitbeans/SimpleDnsCrypt/releases/download/0.4.1/SimpleDNSCrypt.msi' -checksum 'AE36209EE74F8BEB0FAAE909AFA4E4EA099710B77C6EB65CADABFFAD95DF942B' -checksumType 'sha256' 
+Install-ChocolateyPackage -packageName 'simplednscrypt' -FileType msi -SilentArgs '/quiet' -Url 'https://github.com/bitbeans/SimpleDnsCrypt/releases/download/0.4.1/SimpleDNSCrypt.msi' -checksum 'AE36209EE74F8BEB0FAAE909AFA4E4EA099710B77C6EB65CADABFFAD95DF942B' -checksumType 'sha256' 
