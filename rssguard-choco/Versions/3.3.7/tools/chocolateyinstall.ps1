8f38941f2292f761bc72d5ec27bc18efe8856b9e
Zhang Cheng <13501393281@163.com>
2017-01-27 08:41:55 +0000

    update rssguard to 3.3.7

---
+++ rssguard-choco/Versions/3.3.7/tools/chocolateyinstall.ps1
@@ -0,0 +1,4 @@
+﻿
+$ErrorActionPreference = 'Stop'; # stop on all errors
+
+Install-ChocolateyZipPackage -packageName 'rssguard' -UnzipLocation $(Split-Path -Parent $MyInvocation.MyCommand.Definition) -Url 'https://github.com/martinrotter/rssguard/releases/download/3.3.7/rssguard-3.3.7-91edb4d7-win32.7z' -checksum '31DA5DDB8401782DA4D71C1CA675162634966B3319DBE0FB28666C96CA5EFEDE' -checksumType 'sha256'
