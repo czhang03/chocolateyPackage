ba01cb80d122e278c30d2965fdc47c49b75aae04
Zhang Cheng <13501393281@163.com>
2017-02-09 19:40:35 +0000

    update you-get to 0.4.652

--- you-get-choco/Versions/0.4.652/tools/chocolateyinstall.ps1
+++ you-get-choco/Versions/0.4.652/tools/chocolateyinstall.ps1
@@ -1,2 +1,3 @@
 $ErrorActionPreference = 'stop'
 
+Get-ChocolateyWebFile -packageName 'you-get' -FileFullPath "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\you-get.exe" -Url 'https://github.com/soimort/you-get/releases/download/v0.4.652/you-get-0.4.652-win32.exe' -checksum 'FFE79D331712596DD8CF3DFF481C710DF0D03258E36833D802E27D3F6ED371CA' -checksumType 'sha256'
