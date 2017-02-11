84f00545b00fae318d80f1091193a24c3d1e96f1
Zhang Cheng <13501393281@163.com>
2017-02-11 17:22:21 +0000

    update simple dns crypt to 0.4.1

--- simplednscrypt-choco/Versions/0.4.1/tools/chocolateyinstall.ps1
+++ simplednscrypt-choco/Versions/0.4.1/tools/chocolateyinstall.ps1
@@ -1,3 +1,4 @@
 ﻿
 $ErrorActionPreference = 'Stop'; # stop on all errors
 
+Install-ChocolateyZipPackage -packageName 'simplednscrypt' -UnzipLocation $(Split-Path -Parent $MyInvocation.MyCommand.Definition) -Url 'https://github.com/bitbeans/SimpleDnsCrypt/releases/download/0.4.1/SimpleDNSCrypt_0.4.1.zip' -checksum '31D4B8051DDCCD4D43EB84524628E9623F7B24C5FA56CBF51349083962B0861E' -checksumType 'sha256'
