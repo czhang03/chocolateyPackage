dc9e04fafc5e1319ed0a6330a6c2762b0b77acb2
Zhang Cheng <13501393281@163.com>
2017-02-13 13:12:36 +0000

    update visualstudio-github to 2.2.0.7

---
+++ visualstudio-github-choco/Versions/2.2.0.7/tools/chocolateyinstall.ps1
@@ -0,0 +1,2 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+Install-ChocolateyVsixPackage -packageName 'visualstudio-github' -VsixUrl 'https://github.com/github/VisualStudio/releases/download/v2.2.0.7/GitHub.VisualStudio.vsix' -checksum 'DCF2DA04480ED0AAB10BBB9A0F11EC9FEDF2AA291FB6E9A9609DD6E959973B86' -checksumType 'sha256'
