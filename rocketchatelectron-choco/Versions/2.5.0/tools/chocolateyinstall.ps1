ced20a7ed4b3f73d777bec75bd3e041fed904a9b
Zhang Cheng <13501393281@163.com>
2017-03-05 20:34:21 +0000

    update rocket chat to 2.5.0

---
+++ rocketchatelectron-choco/Versions/2.5.0/tools/chocolateyinstall.ps1
@@ -0,0 +1,2 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+Install-ChocolateyPackage -packageName 'rocketchat' -FileType exe -SilentArgs '/S' -Url 'https://github.com/RocketChat/Rocket.Chat.Electron/releases/download/2.5.0/rocketchat-Setup-2.5.0.exe' -checksum '5F71CB436FE3EFE207B82B6AE38B769BFE509ED72CE3B3D83E112050E6F52057' -checksumType 'sha256' 
