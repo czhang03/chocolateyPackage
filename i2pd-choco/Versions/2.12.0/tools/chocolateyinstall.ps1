3d4f6212b4c4cd3aadf5b0c8c7dd8444cfd8d5ea
Zhang Cheng <13501393281@163.com>
2017-03-05 20:40:21 +0000

    refresh i2pd 2.12.0

--- i2pd-choco/Versions/2.12.0/tools/chocolateyinstall.ps1
+++ i2pd-choco/Versions/2.12.0/tools/chocolateyinstall.ps1
@@ -1,4 +1,4 @@
 ﻿
 $ErrorActionPreference = 'Stop'; # stop on all errors
 
-Install-ChocolateyZipPackage -packageName 'i2pd' -UnzipLocation $(Split-Path -Parent $MyInvocation.MyCommand.Definition) -Url 'https://github.com/PurpleI2P/i2pd/releases/download/2.12.0/i2pd_2.12.0_win32_mingw.zip' -checksum '2E272BC45659BB6ACA6EF28702F4DF5F11A2771C991CAB439613A7CF806C3515' -checksumType 'sha256'-Url64 'https://github.com/PurpleI2P/i2pd/releases/download/2.12.0/i2pd_2.12.0_win64_mingw.zip' -checksum64 '8D52281EA38DEFF2F0CB9DF187A8042A62AED182A6E163FAD2E715E67AFC2611' -checksumType64 'sha256'
+Install-ChocolateyZipPackage -packageName 'i2pd' -UnzipLocation $(Split-Path -Parent $MyInvocation.MyCommand.Definition) -Url 'https://github.com/PurpleI2P/i2pd/releases/download/2.12.0/i2pd_2.12.0_win32_mingw.zip' -checksum '9043BA0BA9BA57DCC78BFEB258E0DD422ECEF999B3AE09410C7E5E31F7A6A401' -checksumType 'sha256'-Url64 'https://github.com/PurpleI2P/i2pd/releases/download/2.12.0/i2pd_2.12.0_win64_mingw.zip' -checksum64 '30AE070A6855EFEF2525EC633B9C1337A3BAE0B0DD06DAE6EF486BB8E2A901C3' -checksumType64 'sha256'
