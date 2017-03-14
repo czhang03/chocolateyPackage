dec20db4f736cac8a88f63999eb80871721a2e92
Zhang Cheng <13501393281@163.com>
2017-03-14 15:59:58 +0000

    update git credential manager to 1.9.0

---
+++ Git-Credential-Manager-for-Windows-choco/Versions/1.9.0/tools/chocolateyinstall.ps1
@@ -0,0 +1,3 @@
+﻿
+$ErrorActionPreference = 'Stop'; # stop on all errors
+Install-ChocolateyPackage -packageName 'Git-Credential-Manager-for-Windows' -FileType exe -SilentArgs '/SILENT /NORESTART' -Url 'https://github.com/Microsoft/Git-Credential-Manager-for-Windows/releases/download/v1.9.0/GCMW-1.9.0.exe' -checksum 'F416A29736483735C1A69F9060453FAC0DC3DF6D6BA4A218241EEE22EEEF0F69' -checksumType 'sha256' 
