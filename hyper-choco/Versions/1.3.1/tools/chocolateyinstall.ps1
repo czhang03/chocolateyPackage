fd9fcebe35b98903673f74f5cbfd8436c2c6101f
Zhang Cheng <13501393281@163.com>
2017-03-14 15:58:03 +0000

    update hyper to 1.3.1

---
+++ hyper-choco/Versions/1.3.1/tools/chocolateyinstall.ps1
@@ -0,0 +1,2 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+Install-ChocolateyPackage -packageName 'hyper' -FileType exe -SilentArgs '--silent' -Url64 'https://github.com/zeit/hyper/releases/download/1.3.1/hyper-Setup-1.3.1.exe' -checksum64 'F16AC3F5E104D75DA0DC776AD443BA15FF2A92ABC223968DD6713B62D7FABEEF' -checksumType64 'sha256' 
