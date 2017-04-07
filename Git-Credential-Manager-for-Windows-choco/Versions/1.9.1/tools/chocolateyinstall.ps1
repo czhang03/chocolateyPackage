e33ab6bd4795b6c2fe818f9ace603a7fe29563e6
Zhang Cheng <13501393281@163.com>
2017-04-07 22:12:44 +0800

    update i2pd and git credential manager

---
+++ Git-Credential-Manager-for-Windows-choco/Versions/1.9.1/tools/chocolateyinstall.ps1
@@ -0,0 +1,3 @@
+﻿
+$ErrorActionPreference = 'Stop'; # stop on all errors
+Install-ChocolateyPackage -packageName 'Git-Credential-Manager-for-Windows' -FileType exe -SilentArgs '/SILENT /NORESTART' -Url 'https://github.com/Microsoft/Git-Credential-Manager-for-Windows/releases/download/v1.9.1/GCMW-1.9.1.exe' -checksum '25892E66DEBCD6CC51AE1D92521B3FBE72A2220A3C0B1CBBA54A6F272A3B53FD' -checksumType 'sha256' 
---
+++ i2pd-choco/Versions/2.13.0/tools/chocolateyinstall.ps1
@@ -0,0 +1,4 @@
+﻿
+$ErrorActionPreference = 'Stop'; # stop on all errors
+
+Install-ChocolateyZipPackage -packageName 'i2pd' -UnzipLocation $(Split-Path -Parent $MyInvocation.MyCommand.Definition) -Url 'https://github.com/PurpleI2P/i2pd/releases/download/2.13.0/i2pd_2.13.0_win32_mingw.zip' -checksum '2FF2253092563A4FC5A92F080E0569454AD194CA6ACB702D331366BAD9F8ED48' -checksumType 'sha256'-Url64 'https://github.com/PurpleI2P/i2pd/releases/download/2.13.0/i2pd_2.13.0_win64_mingw.zip' -checksum64 '1B35B6A2AAD32E12CA4ADC09D8CD993E5C8C229382BE07D4028AD0BA34929AF8' -checksumType64 'sha256'
