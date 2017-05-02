9be7b58ea0d0075c4159aa855829bd3cb91555b5
Zhang Cheng <13501393281@163.com>
2017-05-02 22:12:53 +0100

    fix rocketchat and update visualstudio-github

---
+++ visualstudio-github-choco/Versions/2.2.0.10/tools/chocolateyinstall.ps1
@@ -0,0 +1,2 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+Install-ChocolateyVsixPackage -packageName 'visualstudio-github' -VsixUrl 'https://github.com/github/VisualStudio/releases/download/v2.2.0.10/GitHub.VisualStudio.vsix' -checksum '85D8EAABDF20D3457C2228B0C4C91960878F231F9F4A256E39A1F6986C56EFFE' -checksumType 'sha256'
