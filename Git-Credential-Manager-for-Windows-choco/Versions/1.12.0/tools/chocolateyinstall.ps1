942453f031145cc57dfcd4503736a41005b53b5c
chantisnake <13501393281@163.com>
2017-08-07 15:03:26 -0400

    update git-credential-manager to 1.12.0

---
+++ Git-Credential-Manager-for-Windows-choco/Versions/1.12.0/tools/chocolateyinstall.ps1
@@ -0,0 +1,3 @@
+﻿
+$ErrorActionPreference = 'Stop'; # stop on all errors
+Install-ChocolateyPackage -packageName 'Git-Credential-Manager-for-Windows' -FileType exe -SilentArgs '/SILENT /NORESTART' -Url 'https://github.com/Microsoft/Git-Credential-Manager-for-Windows/releases/download/v1.12.0/GCMW-1.12.0.exe' -checksum '1084C986978FDA7D08D8EC6BE60F76B956DA57DF370852D859B9A494A159916B' -checksumType 'sha256' 
