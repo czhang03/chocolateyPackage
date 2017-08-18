71060bca53c6c02f973552c3639ced465dfcc99c
chantisnake <13501393281@163.com>
2017-08-18 05:40:47 -0400

    update

---
+++ i2pd-choco/Versions/2.15.0/tools/chocolateyinstall.ps1
@@ -0,0 +1,4 @@
+﻿
+$ErrorActionPreference = 'Stop'; # stop on all errors
+
+Install-ChocolateyZipPackage -packageName 'i2pd' -UnzipLocation $(Split-Path -Parent $MyInvocation.MyCommand.Definition) -Url 'https://github.com/PurpleI2P/i2pd/releases/download/2.15.0/i2pd_2.15.0_win32_mingw.zip' -checksum '0CE48005E6F1D682A59A99F38423A48F9106E9ACDF1A688844DB275BAF22FD50' -checksumType 'sha256'-Url64 'https://github.com/PurpleI2P/i2pd/releases/download/2.15.0/i2pd_2.15.0_win64_mingw.zip' -checksum64 '9BC0933AD9172379EE9C046BCE7DEC7988286E0F28FF444241657350049F4F04' -checksumType64 'sha256'
---
+++ shadowsocks-choco/Versions/4.0.5/tools/chocolateyinstall.ps1
@@ -0,0 +1,3 @@
+﻿
+$ErrorActionPreference = 'Stop'; # stop on all errors
+Install-ChocolateyZipPackage -packageName 'shadowsocks' -UnzipLocation $(Split-Path -Parent $MyInvocation.MyCommand.Definition) -Url 'https://github.com/shadowsocks/shadowsocks-windows/releases/download/4.0.5/Shadowsocks-4.0.5.zip' -checksum '52327BB54EAA6AFDC2AA3266CFE2C7652570D372E39DA838FB27FABE9FBEA0D0' -checksumType 'sha256'
