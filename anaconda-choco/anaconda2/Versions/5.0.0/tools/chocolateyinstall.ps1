5d4e962e42c710fdeafbce87d40cae4a78d3180e
Liam Kirwan <lkirwan@mwam.com>
2017-10-18 11:13:10 +0100

    parameterise anaconda install

--- anaconda-choco/anaconda2/Versions/5.0.0/tools/chocolateyinstall.ps1
+++ anaconda-choco/anaconda2/Versions/5.0.0/tools/chocolateyinstall.ps1
@@ -5,6 +5,48 @@ $packageName= 'anaconda2'
 $url        = 'http://repo.continuum.io/archive/Anaconda2-5.0.0-Windows-x86.exe'
 $url64      = 'http://repo.continuum.io/archive/Anaconda2-5.0.0-Windows-x86_64.exe'
 $ToolsDir   = Get-ToolsLocation
+
+$pp = Get-PackageParameters
+
+if (!$pp['InstallationType']) {
+    $InstallationType = 'AllUsers'
+} else {
+    if ($pp['InstallationType'] -notin 'AllUsers','JustMe') {
+        Write-Error "Value for InstallationType not recognised: only `'AllUsers`' or `'JustMe`' are valid"
+    } else {
+        $InstallationType = $pp['InstallationType']
+    }
+}
+
+if (!$pp['RegisterPython']) {
+    $RegisterPython = '1'
+} else {
+    if ($pp['RegisterPython'] -notin '0','1') {
+        Write-Error "Value for RegisterPython not recognised: only `'0`' or `'1`' are valid"
+    } else {
+        $RegisterPython = $pp['RegisterPython']
+    }
+}
+
+if (!$pp['AddToPath']) {
+    $AddToPath = '0'
+} else {
+    if ($pp['AddToPath'] -notin '0','1') {
+        Write-Error "Value for AddToPath not recognised: only `'0`' or `'1`' are valid"
+    } else {
+        $AddToPath = $pp['AddToPath']
+    }
+}
+
+if (!$pp['D']) {
+    $D = Join-Path $ToolsDir 'Anaconda2'
+} else {
+    if (!(Test-Path -IsValid $pp['D'])) {
+        Write-Error "Value for D ($($pp['D'])) is not a valid directory path"
+    } else {
+        $D = $pp['D']
+    }
+}
  
 $packageArgs = @{
   packageName   = $packageName
@@ -12,7 +54,7 @@ $packageArgs = @{
   url           = $url
   url64bit      = $url64
  
-  silentArgs    = "/S /InstallationType=AllUsers /RegisterPython=1 /AddToPath=0 /D=$(Join-Path $ToolsDir 'Anaconda2')"
+  silentArgs    = "/S /InstallationType=$InstallationType /RegisterPython=$RegisterPython /AddToPath=$AddToPath /D=$D"
   validExitCodes= @(0)
  
   softwareName  = 'Anaconda2'
