10236c87ba415235ed76ebcc941d298959b2cf19
chantisnake <13501393281@163.com>
2017-11-09 10:31:43 -0500

    update dnSpy to 4.0.0

---
+++ dnSpy-choco/Versions/4.0.0/tools/chocolateyinstall.ps1
@@ -0,0 +1,3 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+Install-ChocolateyZipPackage -packageName 'dnSpy' -UnzipLocation $(Split-Path -Parent $MyInvocation.MyCommand.Definition) -Url 'https://github.com/0xd4d/dnSpy/releases/download/v4.0.0/dnSpy.zip' -checksum 'A985CFABB9297A769595F827FEB76CA2162649479A3D91DB5AA96F611FB57F24' -checksumType 'sha256'
