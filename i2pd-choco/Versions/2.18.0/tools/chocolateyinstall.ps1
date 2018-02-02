718574197ae94234d2f2d77d59a2d0a70fc02925
chantisnake <13501393281@163.com>
2018-02-02 10:34:43 -0500

    update i2pd to 2.18.0

---
+++ i2pd-choco/Versions/2.18.0/tools/chocolateyinstall.ps1
@@ -0,0 +1,4 @@
+﻿
+$ErrorActionPreference = 'Stop'; # stop on all errors
+
+Install-ChocolateyZipPackage -packageName 'i2pd' -UnzipLocation $(Split-Path -Parent $MyInvocation.MyCommand.Definition) -Url 'https://github.com/PurpleI2P/i2pd/releases/download/2.18.0/i2pd_2.18.0_win32_mingw.zip' -checksum 'F006F0EF6580F916FEB0DFDB2B0F439A9616C6B79960EB2537A935B9F5AA4D3C' -checksumType 'sha256'-Url64 'https://github.com/PurpleI2P/i2pd/releases/download/2.18.0/i2pd_2.18.0_win64_mingw.zip' -checksum64 '8100F79F7A3151084C8D2EF18B4B9841E5E0E4AA8EA68FFC6F17AB4B9DDDC3A5' -checksumType64 'sha256'
