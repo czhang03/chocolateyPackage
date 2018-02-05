f2c5d15bc864176a487fb5c6afdd728183ccb131
chantisnake <13501393281@163.com>
2018-02-05 10:38:27 -0500

    update default argument of rocketchat

---
+++ rocketchatelectron-choco/Versions/2.10.2.20180205/tools/chocolateyinstall.ps1
@@ -0,0 +1,2 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+Install-ChocolateyPackage -packageName 'rocketchat' -FileType exe -SilentArgs '/S /allusers' -Url 'https://github.com/RocketChat/Rocket.Chat.Electron/releases/download/2.10.2/rocketchat-setup-2.10.2.exe' -checksum '10B9598DB83542AA484C5767596B67C1AC7277306D0385B739B2CE6F96F8DAD1' -checksumType 'sha256'
