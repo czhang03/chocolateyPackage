7905918b127e926f7d66493af4e5c9b41c96585c
chantisnake <13501393281@163.com>
2018-02-06 11:26:40 -0500

    update rocketchat to 2.10.4

---
+++ rocketchatelectron-choco/Versions/2.10.4/tools/chocolateyinstall.ps1
@@ -0,0 +1,2 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+Install-ChocolateyPackage -packageName 'rocketchat' -FileType exe -SilentArgs '/S /allusers' -Url 'https://github.com/RocketChat/Rocket.Chat.Electron/releases/download/2.10.4/rocketchat-setup-2.10.4.exe' -checksum 'FA846B43F641E0DFD34D52BF20BB11A79516CF266F3669648AF18D9B7DD952AB' -checksumType 'sha256' 
