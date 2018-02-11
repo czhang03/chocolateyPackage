8965aa79b0ba635cceee594f1232f7f7a51aa88e
chantisnake <13501393281@163.com>
2018-02-11 17:38:18 -0500

    update simplednscrypt to 0.4.7

--- simplednscrypt-choco/Versions/0.4.5/tools/chocolateyinstall.ps1
+++ simplednscrypt-choco/Versions/0.4.5/tools/chocolateyinstall.ps1
@@ -1,4 +1,6 @@
 ﻿
-$ErrorActionPreference = 'Stop'; # stop on all errors
+$ErrorActionPreference = "silentlycontinue"
 
-Install-ChocolateyPackage -packageName 'simplednscrypt' -FileType msi -SilentArgs '/quiet' -Url 'https://github.com/bitbeans/SimpleDnsCrypt/releases/download/0.4.5/SimpleDNSCrypt.msi' -checksum 'EE49CC7EF811E026B958AA9545068F79B16A30385325211C8CE76739B0074BDF' -checksumType 'sha256' -Url64 'https://github.com/bitbeans/SimpleDnsCrypt/releases/download/0.4.5/SimpleDNSCrypt64.msi' -checksum64 '19E4058C0A219FA80B96A1AD4A7B143B671ED6B448362C2EB350C5F46A3FE78D' -checksumType64 'sha256' 
+Install-ChocolateyPackage -packageName 'simplednscrypt' -FileType msi -SilentArgs '/quiet /l*vx test.txt' -Url 'https://github.com/bitbeans/SimpleDnsCrypt/releases/download/0.4.5/SimpleDNSCrypt.msi' -checksum 'EE49CC7EF811E026B958AA9545068F79B16A30385325211C8CE76739B0074BDF' -checksumType 'sha256' -Url64 'https://github.com/bitbeans/SimpleDnsCrypt/releases/download/0.4.5/SimpleDNSCrypt64.msi' -checksum64 '19E4058C0A219FA80B96A1AD4A7B143B671ED6B448362C2EB350C5F46A3FE78D' -checksumType64 'sha256'
+
+Get-Content test.txt | Out-Host
---
+++ simplednscrypt-choco/Versions/0.4.7/tools/chocolateyinstall.ps1
@@ -0,0 +1,4 @@
+﻿
+$ErrorActionPreference = 'Stop'; # stop on all errors
+
+Install-ChocolateyPackage -packageName 'simplednscrypt' -FileType msi -SilentArgs '/passive' -Url 'https://github.com/bitbeans/SimpleDnsCrypt/releases/download/0.4.7/SimpleDNSCrypt.msi' -checksum '78161A488BEF815777245B2B78F390EFED93D936E6C4F1D898F6AA0790D23B82' -checksumType 'sha256' -Url64 'https://github.com/bitbeans/SimpleDnsCrypt/releases/download/0.4.7/SimpleDNSCrypt64.msi' -checksum64 '8A8041FE10577759B0B3671144C4252A8CD8C05F991ED6CCF8CF152FAE7BFC9D' -checksumType64 'sha256' 
