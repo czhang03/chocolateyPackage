b74ff1b6f4675bec82db0b46bc27e796bd2c30da
chantisnake <13501393281@163.com>
2018-02-21 16:42:13 -0500

    update shadowsocks to 4.0.8

---
+++ shadowsocks-choco/Versions/4.0.8/tools/chocolateyinstall.ps1
@@ -0,0 +1,3 @@
+﻿
+$ErrorActionPreference = 'Stop'; # stop on all errors
+Install-ChocolateyZipPackage -packageName 'shadowsocks' -UnzipLocation $(Split-Path -Parent $MyInvocation.MyCommand.Definition) -Url 'https://github.com/shadowsocks/shadowsocks-windows/releases/download/4.0.8/Shadowsocks-4.0.8.zip' -checksum 'A44BA10F3E101F1118EA65FF2272E1B2DA2D0AC96CEB0043BF3C9C75AD4A53A7' -checksumType 'sha256'
