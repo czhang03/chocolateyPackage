c40412d4aeeb8a4830572da65b6e9fb06d2ac8c9
chantisnake <13501393281@163.com>
2018-02-27 13:31:48 -0500

    update rssguard to 3.5.6

---
+++ rssguard-choco/Versions/3.5.6/tools/chocolateyinstall.ps1
@@ -0,0 +1,4 @@
+﻿
+$ErrorActionPreference = 'Stop'; # stop on all errors
+
+Install-ChocolateyZipPackage -packageName 'rssguard' -UnzipLocation $(Split-Path -Parent $MyInvocation.MyCommand.Definition) -Url64 'https://github.com/martinrotter/rssguard/releases/download/3.5.6/rssguard-3.5.6-50af26f-win64.7z' -checksum64 '0B8D0A96963441A3B8FB0ADA291BEB963A32DC90D496E15896A96961D6AD9FC5' -checksumType64 'sha256'
