4be13aeb60c597a4a873936b122cc33354bb4874
chantisnake <13501393281@163.com>
2018-08-28 15:04:02 -0400

    update FStar to 0.9.6.0

---
+++ FStar-choco/Versions/0.9.6.0/tools/chocolateyinstall.ps1
@@ -0,0 +1,3 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+Install-ChocolateyZipPackage -packageName 'FStar' -UnzipLocation $(Split-Path -Parent $MyInvocation.MyCommand.Definition) -Url64 'https://github.com/FStarLang/FStar/releases/download/v0.9.6.0/fstar_0.9.6.0_Windows_x64.zip' -checksum64 '566139126C1F8DCB0B2A398E6C3779FA16EE15E2B9C96EA7749DADE5B7F05806' -checksumType64 'sha256'
